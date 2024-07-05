b2a446adaf07a536a078046c0e2eec59430486e6
Ansh Sisodiya <sisodiyaansh785@gmail.com>
2024-07-04 22:53:18 -0400

    Added my project

--- script.js
+++ script.js
@@ -4,56 +4,45 @@ function clockstart(){
     let minute = date.getMinutes();
     let second = date.getSeconds();
 
-    console.log(hour,minute,second)
-    // document.getElementById("hrs").innerText = hours;
-    // document.getElementById("mins").innerText = minute;
-    // document.getElementById("secs").innerText = second;
-    if(hours>12){
-         hours = hours -12;
+    console.log(hour, minute, second);
+    
+    if(hour > 12){
+        hour = hour - 12;
     }
-    let hrs =document.getElementById("hrs");
-    hrs.innerText = hours;
-    let mins =document.getElementById("mins");
-    mins.innerText = minutes;
-    let secs =document.getElementById("secs");
-    secs.innerText = seconds;
+
+    document.getElementById("hrs").innerText = hour;
+    document.getElementById("mins").innerText = minute;
+    document.getElementById("secs").innerText = second;
 }
 
-setInterval(()=>{
-    clockstart();
-},1000)
+setInterval(clockstart, 1000);
 
 let date = new Date();
 let hour = date.getHours();
-if (hour>=12){
-   let ampm = document.getElementById("ampm");
-   ampm.innerText = "Am";
+if (hour >= 12){
+    document.getElementById("ampm").innerText = "PM";
+} else {
+    document.getElementById("ampm").innerText = "AM";
 }
-else if(hours<12){
-    let ampm = document.getElementById("ampm");
-    ampm.innerText = "Pm";
-}   ``
 
 let button = document.getElementById("button");
-button.addEventListener("mouseover",function(){
-    this.innerText = "Party Time"
-})
-button.addEventListener("mouseout",function(){
-    this.innerText = "Set Alarm"
-})
+button.addEventListener("mouseover", function(){
+    this.innerText = "Party Time";
+});
+button.addEventListener("mouseout", function(){
+    this.innerText = "Set Alarm";
+});
 
 let greeting = document.getElementById("greeting");
-if(hours>=12 && hours<16){
-    greeting.innerHTML = "Good Afternoon"
-}
- if(hours>=16 && hours<=20){
-        greeting.innerHTML = "Good Evening"
-}
- if(hours>=20 && hour<=24){
-    greeting.textContent = "Good Night"
+if(hour >= 12 && hour < 16){
+    greeting.innerHTML = "Good Afternoon";
+} else if(hour >= 16 && hour <= 20){
+    greeting.innerHTML = "Good Evening";
+} else if(hour >= 20 && hour <= 24){
+    greeting.textContent = "Good Night";
 }
 
-button.addEventListener("click",function(){
+button.addEventListener("click", function(){
     let date = new Date();
     let hour = date.getHours();
 
@@ -62,26 +51,23 @@ button.addEventListener("click",function(){
     let nap = document.getElementById("nap");
     let night = document.getElementById("night");
 
-    if(parseInt(wakeUp.value)=== hours){
-        document.getElementById("settime").innerHTML = "Grab some Breakfast !";
+    if(parseInt(wakeUp.value) === hour){
+        document.getElementById("settime").innerHTML = "Grab some Breakfast!";
         document.getElementById("clockimage").src = "images/Component 30 – 1.svg";
-    }
-    else if(parseInt(lunch.value)=== hours){
-        document.getElementById("settime").innerHTML = "Let's have some Lunch !";
+    } else if(parseInt(lunch.value) === hour){
+        document.getElementById("settime").innerHTML = "Let's have some Lunch!";
         document.getElementById("clockimage").src = "images/Component 31 GÇô 1.jpg";
-    }
-    else if(parseInt(nap.value)=== hours){
+    } else if(parseInt(nap.value) === hour){
         document.getElementById("settime").innerHTML = "Get some Tea!";
         document.getElementById("clockimage").src = "images/lunch_time.svg";
-    }
-    else if(parseInt(night.value)=== hours){
+    } else if(parseInt(night.value) === hour){
         document.getElementById("settime").innerHTML = "Go to Sleep";
-        document.getElementById("clockimage").src = "images/goodnight_image.svg ";
+        document.getElementById("clockimage").src = "images/goodnight_image.svg";
     }
 
-   let timing =  document.getElementsByClassName("timing");
-   timing[0].innerText = wakeUp.options[wakeUp.selectedIndex].text;
-   timing[1].innerText = lunch.options[lunch.selectedIndex].text;
-   timing[2].innerText = wakeUp.options[nap.selectedIndex].text;
-   timing[3].innerText = wakeUp.options[night.selectedIndex].text;
-})
+    let timing = document.getElementsByClassName("timing");
+    timing[0].innerText = wakeUp.options[wakeUp.selectedIndex].text;
+    timing[1].innerText = lunch.options[lunch.selectedIndex].text;
+    timing[2].innerText = nap.options[nap.selectedIndex].text;
+    timing[3].innerText = night.options[night.selectedIndex].text;
+});
